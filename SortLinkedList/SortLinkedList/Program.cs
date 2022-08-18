f443f08a355369a34fb7401e1ac612c2b836aa0d
Erfan Faridi <109587086+Erfan-ffa@users.noreply.github.com>
2022-08-18 01:46:20 -0700

    Add files via upload

---
+++ SortLinkedList/SortLinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SortLinkedList;
+
+Console.WriteLine("Hello, World!");
+Operation x = new Operation();
+
+x.Enqueue(1);
+x.Enqueue(2);
+x.Enqueue(2);
+x.Enqueue(1);
+x.Enqueue(8);
+
+x.SortLinkedList();
+
+
+x.Print();
